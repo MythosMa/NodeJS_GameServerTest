a968ae3c5467ec033c10cf1c64efdce05433148d
MythosMa <mythosmayuan@gmail.com>
2019-12-26 17:14:21 +0800

    update code

--- Server/MapServerModules/mapObject.js
+++ Server/MapServerModules/mapObject.js
@@ -1,14 +1,11 @@
-const playerObj = require("../../modules/PlayerObject");
-
 class MapObject {
     constructor(mapName) {
-        this.mapName = mapName;
-        this.npcs = [];
-        this.players = [];
+        this.mapName = mapName;//地图名称
+        this.npcs = [];//地图中npc的数组
+        this.players = [];//地图中玩家的数组
     }
-    init() {
 
-    }
+    //客户端连接完毕，创建玩家的数据信息后将其放入地图的玩家数组中
     addPlayer(player) {
         for(let i in this.players) {
             let playerItem = this.players[i];
@@ -18,15 +15,18 @@ class MapObject {
         }
         this.players.push(player);
     }
+    /**
+     * 监听到玩家离开地图，要从数组中将玩家信息删除
+     * 因为玩家离开后，需要通知其他还在连接中的玩家
+     * 所以延迟从数组中删掉，是为了给其他玩家发送地图玩家数据时标记玩家退出
+     */
     deletePlayer(player) {
-        player.end();
         setTimeout(() => {
             this.players.splice(this.players.indexOf(player), 1);
         }, 1000);
     }
-    sayHello() {
-        console.log("hello -> " + this.mapName);
-    }
+
+    //地图信息，将npc和玩家数据打包成数据集
     getMapInfo() {
         return {
             npcs: this.npcs.map((item) => {return null}),
--- Server/mapServer.js
+++ Server/mapServer.js
@@ -1,19 +1,23 @@
-const wsServer = require("../modules/websocketServer");
-const mapObject = require("./MapServerModules/mapObject");
-const playerObject = require("../modules/PlayerObject");
+const wsServer = require("../modules/websocketServer");//引入websocketServer
+const mapObject = require("./MapServerModules/mapObject");//引入地图类
+const playerObject = require("../modules/playerObject");//引入角色类
 
-const map_1 = new mapObject("map_1");
+const map_1 = new mapObject("map_1");//初始化地图信息
 
+//同聊天服务器一样，几个回调函数来处理连接的监听
 const textCallback = (server, result, connection) => {
   let dataType = result.dataType;
   let player = null;
-  let responseData = {
-    dataType: "",
-    data: ""
-  };
 
+  //通过dataType的操作指令，执行不同的任务
+  /**
+   * PLAYER_SERVER_INIT 服务器角色初始化，用于客户端连接成功后，向服务器发送的命令
+   * PLAYER_SERVER_INIT_OVER 服务器完成角色初始化，向客户端发送命令，要求客户端创建角色相关数据
+   * PLAYER_CLIENT_INIT_OVER 客户端角色创建完成，通知服务器开始角色数据计算
+   * PLAYER_OPERATION 客户端发起操作，控制角色，改变角色状态
+   */
   switch (dataType) {
-    case "PLAYER_SERVER_INIT":
+    case "PLAYER_SERVER_INIT"://服务器角色初始化，并添加连接对象
       player = new playerObject();
       player.addConnection(connection);
       connection["player"] = player;
@@ -22,26 +26,30 @@ const textCallback = (server, result, connection) => {
         makeResponseData("PLAYER_SERVER_INIT_OVER", player.getPlayerData())
       );
       break;
-    case "PLAYER_CLIENT_INIT_OVER":
+    case "PLAYER_CLIENT_INIT_OVER"://客户端角色创建完成，开启角色数据计算，并将角色信息添加到地图信息中心
       player = connection.player;
       player.start();
       map_1.addPlayer(player);
       break;
-    case "PLAYER_OPERATION":
+    case "PLAYER_OPERATION"://客户端发来的操作，改变角色状态
       player = connection.player;
       player.operation(result.data);
       break;
   }
 };
 
+//通知客户端连接成功
 const connectCallback = (server, result, connection) => {
   connection.sendText(makeResponseData("CONNECT_SUCCESS", null));
 };
 
+//客户端取消连接，将该客户端角色移出地图数据
 const closeConnectCallback = (server, result, connection) => {
+  connection.player.end();
   map_1.deletePlayer(connection.player);
 };
 
+//打包数据的公共函数
 const makeResponseData = (dataType, data) => {
   return JSON.stringify({
     dataType,
